df098bd406245ce6f0fa57f92e60e180565eac3c
Elizabeth <ebowen19@g.ucla.edu>
2024-01-24 18:39:06 -0800

    tif to jpg

---
+++ scripts/tif_to_jpg.py
@@ -0,0 +1,27 @@
+import os
+from PIL import Image
+
+folder = '/Users/elizabeth 1/Library/CloudStorage/Box-Box/Elizabeth/MSSR-Statin-Project/images'
+
+#%%
+for filename in os.listdir(folder):
+    if filename.endswith(".tif"):
+        # Construct the full file path for the TIFF file
+        file_path = os.path.join(folder, filename)
+        # Extract filename without extension
+        file_name_without_ext = os.path.splitext(filename)[0]
+        # Define the new filename with .jpg extension
+        new_file_path = os.path.join(folder, file_name_without_ext + ".jpg")
+
+        # Check if the JPEG version already exists
+        if not os.path.exists(new_file_path):
+            # Open the image
+            with Image.open(file_path) as img:
+                # Convert to RGB (JPEG does not support alpha channel as in PNG)
+                rgb_img = img.convert('RGB')
+                # Save the image as JPEG
+                rgb_img.save(new_file_path, "JPEG")
+        else:
+            print(f"JPEG version of '{filename}' already exists. Skipping conversion.")
+
+print("Conversion complete.")
